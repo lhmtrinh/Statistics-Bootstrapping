c14ad83b0163c2742f2ac946ced6b7db7d4b6cac
trinhlehoangminh2000 <trinhlehoangminh2000@gmail.com>
2022-11-16 02:11:58 +0100

    Add print out for each cell in the livescript

--- ES.m
+++
@@ -1,6 +0,0 @@
-function es = ES(param,alpha)
-    c01 = tinv(alpha , param(1)); % left tail quantile, for loc-0 scale-1
-    es = -tpdf(c01,param(1))/tcdf(c01,param(1)) * (param(1)+c01^2)/(param(1)-1);
-    es = param(2)+param(3)*es;
-end
-
